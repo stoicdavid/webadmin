6619a744b25707cd204aedfaf134b3472fd3442c
David Rodriguez <davidrodriguez@david-rodriguezs-macbook.local>
2009-01-17 19:10:18 -0600

    correo2

--- app/models/neuro_mailer.rb
+++ app/models/neuro_mailer.rb
@@ -10,15 +10,20 @@ class NeuroMailer < ActionMailer::Base
   end
 
   def informa_paciente(paciente,estudio,cita,doctor)
-    subject    "Indicaciones para #{estudio.tipo_estudio}"
+    subject    "Indicaciones para #{estudio.tipo_estudio} de #{paciente.nombre_completo}"
     recipients paciente.correo
     from       "Rocío Valdez Dávalos <rvd@americanneurolab.com>"
     sent_on    Time.now
     content_type "text/html"
     body       :paciente => paciente,:estudio => estudio,:cita => cita,:doctor => doctor
-    
-    ruta = "#{RAILS_ROOT}/public/docs/instrucciones_previas.pdf"
-    
+    ruta = ""
+    if paciente.edad(Time.now) < 3
+      ruta = "#{RAILS_ROOT}/public/docs/instrucciones_previas_inf.pdf"
+    elsif paciente.edad(Time.now) < 21
+      ruta = "#{RAILS_ROOT}/public/docs/instrucciones_previas_teen.pdf"
+    else
+      ruta = "#{RAILS_ROOT}/public/docs/instrucciones_previas_adulto.pdf"
+    end
     attachment :content_type  => "application/pdf",
                :body =>  File.read(ruta),
                :filename  =>  "instrucciones.pdf"
